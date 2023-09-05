df3f6790ce883273137d23dd92b5c721c2a96488
Abhi M <monkeypunkey79@gmail.com>
2023-09-05 01:26:08 -0700

    TrieNode class

--- src/Trie.ts
+++ src/Trie.ts
@@ -1,2 +1,18 @@
 import {Pair, TickerData} from '../types'
+import tickers from './data/tickers.json'
+
+const tickerPairs: Array<[number, TickerData]> = Object.entries(tickers).map(([index, data]) => [Number(index), data as TickerData]);
+
+class TrieNode {
+    children: { [key: string]: TrieNode };
+    endOfWord: boolean;
+    pairs: Pair[];
+
+    constructor() {
+        this.children = {};
+        this.endOfWord = false;
+        this.pairs = [];
+    }
+}
+
 
